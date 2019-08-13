06b6409690877e5166b959022989f88e681b3295
Sebastian Skoczylas <s.skoczylas92@gmail.com>
2019-08-13 19:10:00 +0200

    add first step of the task

--- js/script.js
+++ js/script.js
@@ -32,11 +32,68 @@ const titleClickHandler = function(event){
     const targetArticle = document.querySelector(articleSelector)
     console.log(targetArticle)
 
-  /* add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
   targetArticle.classList.add('active');
 }
 
+
+
+/*
+    NEXT EXERICSE
+
+    Generate Title links
+
+ */
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /*  [DONE] remove contents of titleList */
+
+  const optTitleListSelector = document.querySelector('.titles');
+	optTitleListSelector.innerHTML = '';
+
+  /* {DONE] for each article */
+
+  const articles = document.querySelectorAll('.post');
+
+  let html = '';
+
+  for(let article of articles);{
+
+    /* [DONE] get the article id */
+
+    const articleId = article.getAttribute("id"); /*pokazuje ze nie jest zdefiniowana*/
+		console.log('articleId');
+
+    /* {DONE] find the title element */
+    /* {DONE] get the title from the title element */
+
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* [DONE] create HTML of the link */
+
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log('link');
+
+
+    /* [DONE] insert link into titleList */
+
+    titleList.insertAdjacentHTML('beforeend, linkHTML');
+
+    html = html + linkHTML;
+    console.log('');
+}
+    titleList.innerHTML = html;
+}
+
+generateTitleLinks();
+
 const links = document.querySelectorAll('.titles a');
+  console.log('links');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
